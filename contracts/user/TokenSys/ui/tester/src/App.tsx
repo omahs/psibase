8ac095852ae4beb8966f313c059b708e888b1efa
sparkplug0025 <sparkplug0025@gmail.com>
2022-08-28 09:32:46 -0400

    playing with components wip

--- contracts/user/TokenSys/ui/tester/src/App.tsx
+++ contracts/user/TokenSys/ui/tester/src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import wait from 'waait'
+import { useState } from "react";
 
-import { initializeApplet, action, query, operation, siblingUrl, getJson, setOperations } from "common/rpc.mjs"
-import useEffectOnce from './hooks/useEffectOnce'
+import { Button } from "@toolbox/components";
+
+import "./App.css";
+import wait from "waait";
+
+import {
+  initializeApplet,
+  action,
+  query,
+  operation,
+  siblingUrl,
+  getJson,
+  setOperations,
+} from "common/rpc.mjs";
+import useEffectOnce from "./hooks/useEffectOnce";
 
 interface Uint64 {
   value: string;
@@ -17,49 +27,59 @@ interface FungibleToken {
       dailyLimitPct: string;
       dailyLimitQty: string;
       yearlyLimitPct: string;
-    },
+    };
     stats: {
       avgDaily: string;
       avgYearly: string;
-    }
-  },
+    };
+  };
   config: {
-    bits: number
-  },
+    bits: number;
+  };
   precision: {
     value: number;
-  },
-  currentSupply: Uint64,
+  };
+  currentSupply: Uint64;
   maxSupply: Uint64;
   symbolId: string;
 }
 
 interface TokenBalance {
-  account: string; balance: string; precision: number, token: number, symbol: string
+  account: string;
+  balance: string;
+  precision: number;
+  token: number;
+  symbol: string;
 }
 
 class Contract {
-  protected cachedApplet?: string
+  protected cachedApplet?: string;
 
   protected async applet(): Promise<string> {
     if (this.cachedApplet) return this.cachedApplet;
-    const appletName = await getJson('/common/thiscontract') as string;
+    const appletName = (await getJson("/common/thiscontract")) as string;
     this.cachedApplet = appletName;
     return appletName;
   }
-
 }
 
 class TokenContract extends Contract {
-
   public async fetchTokenTypes() {
-    const url = await siblingUrl(undefined, await this.applet(), "api/getTokenTypes");
+    const url = await siblingUrl(
+      undefined,
+      await this.applet(),
+      "api/getTokenTypes"
+    );
     return getJson<FungibleToken[]>(url);
   }
 
-  public async fetchBalances(user: string = 'alice') {
-    const url = await siblingUrl(undefined, 'token-sys', `api/balances/${user}`)
-    return getJson<TokenBalance[]>(url)
+  public async fetchBalances(user: string = "alice") {
+    const url = await siblingUrl(
+      undefined,
+      "token-sys",
+      `api/balances/${user}`
+    );
+    return getJson<TokenBalance[]>(url);
   }
 
   // idea:
@@ -67,28 +87,36 @@ class TokenContract extends Contract {
   // async credit ({ symbol, receiver, amount, memo }: { symbol: string, receiver: string, amount: string, memo: string }) => {
 
   // })
-
 }
 
 const tokenContract = new TokenContract();
 
-
 function App() {
-
   useEffectOnce(() => {
     initializeApplet(async () => {
-      const thisApplet = await getJson('/common/thiscontract') as string;
+      const thisApplet = (await getJson("/common/thiscontract")) as string;
 
       setOperations([
         {
           id: "credit",
-          exec: async ({ symbol, receiver, amount, memo }: { symbol: string, receiver: string, amount: string, memo: string }) => {
-
-            console.log("credit hit!")
+          exec: async ({
+            symbol,
+            receiver,
+            amount,
+            memo,
+          }: {
+            symbol: string;
+            receiver: string;
+            amount: string;
+            memo: string;
+          }) => {
+            console.log("credit hit!");
 
             //TODO: let tokenId = query("symbol-sys", "getTokenId", {symbol});
             const allTokens = await tokenContract.fetchTokenTypes();
-            const token = allTokens.find(t => t.symbolId === symbol.toLowerCase());
+            const token = allTokens.find(
+              (t) => t.symbolId === symbol.toLowerCase()
+            );
             if (!token) {
               console.error("No token with symbol " + symbol);
               return;
@@ -97,72 +125,77 @@ function App() {
             const value = String(Number(amount) * Math.pow(10, 8));
 
             try {
-              await action(thisApplet, "credit",
-                {
-                  tokenId: token.id,
-                  receiver,
-                  amount: { value },
-                  memo: { contents: memo },
-                });
+              await action(thisApplet, "credit", {
+                tokenId: token.id,
+                receiver,
+                amount: { value },
+                memo: { contents: memo },
+              });
             } catch (e) {
-              console.error('tx failed', e)
+              console.error("tx failed", e);
             }
           },
-        }
+        },
       ]);
     });
 
     query("account-sys", "", "getLoggedInUser", {}, (loggedInUser: any) => {
-      console.log('getLoggedInUser:', loggedInUser)
+      console.log("getLoggedInUser:", loggedInUser);
     });
     wait(1000).then(() => {
-      getBalance().then(setUserBalance)
-    })
-  }, [])
+      getBalance().then(setUserBalance);
+    });
+  }, []);
 
-  const [userBalance, setUserBalance] = useState('');
-
+  const [userBalance, setUserBalance] = useState("");
 
   const getBalance = async (current?: string, attempt = 1): Promise<string> => {
     const res = await tokenContract.fetchBalances();
-    const parsedBalance = Number(Number(res[0].balance) / Math.pow(10, 8)).toString();
+    const parsedBalance = Number(
+      Number(res[0].balance) / Math.pow(10, 8)
+    ).toString();
 
     const maxAttempts = 5;
     const tooManyAttempts = attempt > maxAttempts;
     if (!current || parsedBalance !== current || tooManyAttempts) {
       if (tooManyAttempts) {
-        console.warn(`getBalance failed to return a different balance than the previously given ${current} after ${maxAttempts}`);
+        console.warn(
+          `getBalance failed to return a different balance than the previously given ${current} after ${maxAttempts}`
+        );
       }
-      return parsedBalance
-    };
-    console.log(`awaiting before checking balance again... Attempt: ${attempt}`);
+      return parsedBalance;
+    }
+    console.log(
+      `awaiting before checking balance again... Attempt: ${attempt}`
+    );
     await wait(500);
     return getBalance(current, attempt + 1);
-  }
-
-  const triggerTx = async () => {
-    const beforeBalance = userBalance
-    operation('token-sys', '', 'credit', { symbol: 'PSI', receiver: 'bob', amount: 3.5, memo: 'Working' })
-    await wait(2000);
-    const balance = await getBalance(beforeBalance)
-    console.log('balances fetched', balance)
-    setUserBalance(balance)
   };
 
-
+  const triggerTx = async () => {
+    const beforeBalance = userBalance;
+    operation("token-sys", "", "credit", {
+      symbol: "PSI",
+      receiver: "bob",
+      amount: 3.5,
+      memo: "Working",
+    });
+    await wait(2000);
+    const balance = await getBalance(beforeBalance);
+    console.log("balances fetched", balance);
+    setUserBalance(balance);
+  };
 
   return (
     <div className="App">
-
       <h1>Alices balance is {userBalance}</h1>
       <div className="card">
-        <button onClick={() => { triggerTx() }}>
-          send 3.5 tokens to bob
-        </button>
+        <Button size="lg" onClick={triggerTx}>
+          send 3.5 tokens to booob
+        </Button>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
